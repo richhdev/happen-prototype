9f5214a422bfd22619cd18b764e981abc58ce3b9
richhdev <79666708+richhdev@users.noreply.github.com>
2026-09-07 10:46:27 +1000

    update vendor assets

--- components/Vendors/data.js
+++ components/Vendors/data.js
@@ -4,14 +4,14 @@ import { asset } from "@/lib/data";
 export const VENDOR_EVENTS = [
   {
     name: "Good Things Festival",
-    img: asset("/assets/market-stall-good-things.png"),
+    img: asset("/assets/vendor-good-things.webp"),
     logo: asset("/assets/client-good-things.svg"),
     cta: "Get your stall",
     link: "https://form.jotform.com/261311126413846",
   },
   {
     name: "Beyond The Valley",
-    img: asset("/assets/market-stall-btv-precinct.jpg"),
+    img: asset("/assets/vendor-beyond-the-valley.webp"),
     logo: asset("/assets/client-beyond-the-valley.svg"),
     cta: "Get your stall",
     link: "https://form.jotform.com/261448233625861",
