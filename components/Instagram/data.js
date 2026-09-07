092c3adc1d93e298c6ccb3d3a5285eb6645b8168
richhdev <79666708+richhdev@users.noreply.github.com>
2026-09-07 13:44:04 +1000

    add optimised instagram photo

--- components/Instagram/data.js
+++ components/Instagram/data.js
@@ -3,10 +3,10 @@ import { asset } from "@/lib/data";
 export const IG_PROFILE = "https://www.instagram.com/happengroupau/";
 
 export const IG_TILES = [
-  asset("/uploads/insta1.jpg"),
-  asset("/uploads/insta2.png"),
-  asset("/uploads/insta3.jpg"),
-  asset("/uploads/insta4.jpg"),
-  asset("/uploads/insta5.png"),
-  asset("/uploads/insta6.jpg"),
+  asset("/assets/insta-1.webp"),
+  asset("/assets/insta-2.webp"),
+  asset("/assets/insta-3.webp"),
+  asset("/assets/insta-4.webp"),
+  asset("/assets/insta-5.webp"),
+  asset("/assets/insta-6.webp"),
 ];
